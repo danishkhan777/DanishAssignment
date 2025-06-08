3edf716c54345f3af3135eef192ce9fda2803fb5
danishkhan777 <kdanish2608@gmail.com>
2025-06-08 18:31:35 +0530

    16Th Question

---
+++ src/Assignment_June7/Q16.kt
@@ -0,0 +1,10 @@
+package Assignment_June7
+
+fun main()
+{
+    val ordername = "Latte"
+    if(ordername.length >= 5)
+        println("Thats a Perferct Name for Coffee")
+    else
+        println("Too Long !! How Do Yopu Remember That")
+}
