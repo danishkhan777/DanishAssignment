8639487ea3a8c9ed5ad905377e95a6f22b6b7ce9
danishkhan777 <kdanish2608@gmail.com>
2025-06-08 17:44:35 +0530

    11TH Question

---
+++ src/Assignment_June7/Q11.kt
@@ -0,0 +1,12 @@
+package Assignment_June7
+
+fun main()
+{
+    var strength=0
+    while(strength < 81)
+    {
+        println("Strength of Coffee is $strength")
+        strength +=1
+    }
+    println("Maximum strength Reached")
+}
