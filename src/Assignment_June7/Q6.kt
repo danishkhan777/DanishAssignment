8d07adda61030ab9651be9b375222157b8cd5781
danishkhan777 <kdanish2608@gmail.com>
2025-06-08 14:24:29 +0530

    6Th Question

---
+++ src/Assignment_June7/Q6.kt
@@ -0,0 +1,17 @@
+package Assignment_June7
+
+fun main() {
+    print("Enter a day number (1-7, where 1=Monday): ")
+    val dayNum = readln().toInt()
+
+    when (dayNum) {
+        1 -> println("Double Espresso")
+        2 -> println("Cappuccino")
+        3 -> println("Cold Brew")
+        4 -> println("Flat White")
+        5 ->println( "Iced Latte")
+        6 -> println(" Hot Latte")
+        7 ->println( "Decaf Herbal Tea")
+        else -> println("Invalid Day Number")
+    }
+}
