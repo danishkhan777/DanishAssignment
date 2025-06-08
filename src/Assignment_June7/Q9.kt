6a40523e8e2b522a550507fbc63b6ee1b1a39f19
danishkhan777 <kdanish2608@gmail.com>
2025-06-08 14:44:53 +0530

    9Th Question

--- src/Assignment_June7/Q9.kt
+++ src/Assignment_June7/Q9.kt
@@ -1,2 +1,11 @@
 package Assignment_June7
 
+fun main()
+{
+    print("Enter your age: ")
+    val age = readln().toInt()
+        if (age >= 60)
+            println("✅ Senior discount unlocked! Enjoy The special discount")
+        else
+            println("Hey There Young Mind ! Hope You Like The Coffee")
+}
