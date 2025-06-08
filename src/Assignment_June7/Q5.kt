ae25427e684d3bd4e812ce0f71285aaf9f2bb6dc
danishkhan777 <kdanish2608@gmail.com>
2025-06-08 14:15:10 +0530

    5TH Question

---
+++ src/Assignment_June7/Q5.kt
@@ -0,0 +1,11 @@
+package Assignment_June7
+
+fun main() {
+    val orderNum = 43
+
+    if (orderNum % 2 == 0) {
+        println("Order No : $orderNum !! Shall We Call it Even !")
+    } else {
+        println("Order No : $orderNum !! Your Orfer number must be odd but your coffee wont be !!")
+    }
+}
